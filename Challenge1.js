f480d0bea2b56b2528a46583cd31fe6c42252895
Christine Trant <christinetrant@gmail.com>
2020-05-10 16:06:31 +0100

    deleted round 2 file as moved working code into main file.  added a sort array function at beginning to get required output

--- Challenge1.js
+++ Challenge1.js
@@ -1,68 +1,53 @@
 // **Question 1**: Clean the room function: given an input of `[1,2,4,591,392,391,2,5,10,2,1,1,1,20,20]`, make a function that organizes these into individual array that is ordered. For example `answer(ArrayFromAbove)` should return: `[[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]`. Bonus: Make it so it organizes strings differently from number types. i.e. `[1, "2", "3", 2]` should return `[[1,2], ["2", "3"]]`
 
-// // let array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-// let array = [1,2,45,391,2,1,20,20];
-// // 1. sort array in ascending order:
-// // NOT SURE IF THIS MATTERS?!
-// array = array.sort((a, b) => a-b);
-// let newArr = [];
-
-// for(let j=0; j<array.length; j++) {
-// 	// Check array to add similar value to array
-// 	let tempArr = [];
-// 	for(let i=0; i<array.length; i++) {
-// 		let currentValue = array[0];
-// 		if(currentValue === array[i]) {
-// 			tempArr.push(currentValue);	
-// 		}
-// 	}
-// 	// return tempArr
-// 	console.log('tempArr', tempArr);
-// 	// Add our temporarily created array with set of duplicates to the new array
-// 	newArr.push(tempArr)
-
-// 	// Then need to remove those numbers from original array
-// 	array.splice(0, tempArr.length)
-// 	console.log('array', array);
-// 	console.log('new array', newArr);
-
-// 	// If number isn't matching just add to array
-// 	array.concat(newArr)
-// }
+let array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+// sort array in ascending order:
+array = array.sort((a, b) => a-b);
+// Create a new array so we don't tamper with original
+let newArr = [];
+// We will need a temporary array to store instances of matching values
+let tempArr = [];
 
 
-
-// let array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-let testarray = [1,2,45,391,2,1,20,20];
-// 1. sort array in ascending order:
-// NOT SURE IF THIS MATTERS?!
-	
-function cleanFunction(array) {	
-	array = array.sort((a, b) => a-b);
-	let newArr = [];
-
-	for(let j=0; j<array.length; j++) {
-		// Check array to add similar value to array
-		let tempArr = [];
-		for(let i=0; i<array.length; i++) {
-			let currentValue = array[0];
-			if(currentValue === array[i]) {
-				tempArr.push(currentValue);	
-			}
+let arrayFind = (arr => {
+	arr.forEach((item) => {
+		let currentValue = arr[0];
+		if(currentValue === item) {
+			tempArr.push(currentValue);	
 		}
-		// return tempArr
-		console.log('tempArr', tempArr);
-		// Add our temporarily created array with set of duplicates to the new array
-		newArr.push(tempArr)
-
-		// Then need to remove those numbers from original array
-		array.splice(0, tempArr.length)
-		console.log('array', array);
-		console.log('new array', newArr);
-
-		
+		currentValue = arr[i]
+	})
+	// Add to a new array
+	if(tempArr.length>1) {
+		return newArr.push(tempArr);
+	} else {
+		return newArr = newArr.concat(tempArr);
 	}
-	// If number isn't matching just add to array
-	array =	array.concat(newArr)
-	console.log('final', array);
+})
+
+// Remove from array function
+// SO ONCE WE KNOW MATCHING ITEM e.g. tempArr[0] we can filter and remove:
+let arrayRemove = (arr, value) => { 
+	return arr.filter((item) => { 
+		// return all elements that are not in tempArr
+		return item != value; 
+	});
 }
+
+// Check if value repeats in array
+let cleanArray = (arr) =>{
+	arr.forEach((item) => {
+		if(arr.length>0) {
+			arrayFind(arr);
+			// Before we clear tempArr need to remove those values from original array
+			arr = arrayRemove(arr, tempArr[0]);
+			// clear tempArr
+			tempArr = [];
+			// console.log(newArr);
+		}
+	})
+	return newArr;
+}
+
+
+cleanArray(array);
