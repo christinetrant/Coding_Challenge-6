46c05fc4545627157dfc43657e1573d5d5690196
Christine Trant <christinetrant@gmail.com>
2020-05-10 20:40:57 +0100

    Tidied up code and commented so it makes sense when I come back to it

--- Challenge2.js
+++ Challenge2.js
@@ -1,38 +1,30 @@
 // **Question 2**: Write a javascript function that takes an array of numbers and a target number. The function should find two different numbers in the array that, when added together, give the target number. For example: `answer([1,2,3], 4)` should return `[1,3]`
 
 let array = [5, 6, 2, 2, 3, 4];
-let sum = [];
+
 let findTwoNumbers = (arr, target) => {
-	let firstNum, numsToFind;
-	
-for(item of arr) {
-// for(let j=0; j<arr.length; j++) {
-	firstNum = item;
-	arr.map(() => {
-		// console.log('value', value);
-		// console.log('i', i);
-		// console.log('arr[0]', arr[0]);
-		numToFind = target-firstNum;
+	let firstNum, secondNum, numsToFind, sum;
+		for(item of arr) {
+			// sum is an empty array
+			sum = [];
+			// the first item in our array equals the first number
+			firstNum = item;
+			
+			// subtract the first number from target to get the second number we need to look for
+			arr.map(() => numToFind = target-firstNum)
 
-	})
+			// check to see if the second number exists in array
+			secondNum = arr.includes(numToFind)
 
-	// let secondNum = arr.filter(num => num === numToFind);
-	let secondNum = arr.includes(numToFind)
-
-	console.log('second', secondNum)
-	if(secondNum === true) {
-		console.log('number exists')
-		sum.push(firstNum);
-		console.log('sum1', sum)
-		sum.push(numToFind);
-		console.log('sum2', sum)
-		return sum;
-	} else {
-		console.log('no matches')
-		firstNum = item++
-		// sum = [];
-	}
-}
-
-	
+			// if it exists then push both numbers into the sum array and return
+			if(secondNum === true) {
+				sum.push(firstNum);
+				sum.push(numToFind);
+				return sum;
+			// otherwise first number equals the next value in array to try
+			} else {
+				firstNum = item++
+				return `No numbers in this array add up to ${target}`
+			}
+		}	
 }
