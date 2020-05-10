02090f4a75678170fa34068e4599fd2b6d641a24
Christine Trant <christinetrant@gmail.com>
2020-05-10 20:48:54 +0100

    adding challenge 3 - not yet started just added a few notes

---
+++ Challenge3.js
@@ -0,0 +1,14 @@
+// **Question 3**: Write a function that converts HEX to RGB. Then Make that function autodect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX. 
+// Bonus: Release this tool as a npm package.
+
+// HEX
+// 6 digits #rrggbb
+// 00 to FF
+// 0123456789ABCDEF
+// If any one of the three color values is less than 10 hex (16 decimal), it must be represented with a leading zero so that the triplet always has exactly six digits.
+
+// https://en.wikipedia.org/wiki/Web_colors
+
+// RGB
+// (rr, gg, bb) 
+// 0 to 255
