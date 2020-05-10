46a01b124c3cca8a61370ce7a41664747f56fe3f
Christine Trant <christinetrant@gmail.com>
2020-05-10 17:44:50 +0100

    Function can seperate numbers and strings into 2 arrays and then organise, will work if only one type is in the array also - Bonus bit completed :)

--- Challenge1.js
+++ Challenge1.js
@@ -1,9 +1,7 @@
 // **Question 1**: Clean the room function: given an input of `[1,2,4,591,392,391,2,5,10,2,1,1,1,20,20]`, make a function that organizes these into individual array that is ordered. For example `answer(ArrayFromAbove)` should return: `[[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]`. Bonus: Make it so it organizes strings differently from number types. i.e. `[1, "2", "3", 2]` should return `[[1,2], ["2", "3"]]`
 
-let array = [1, "2", "3", 2];
-// let array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-// sort array in ascending order:
-array = array.sort((a, b) => a-b);
+let array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+let array2 = [1, "2", "3", 2];
 // Create a new array so we don't tamper with original
 let newArr = [];
 // We will need a temporary array to store instances of matching values
@@ -11,7 +9,7 @@ let tempArr = [];
 let strArr = [];
 let numArr = [];
 
-
+// Find matching values in array
 let arrayFind = (arr => {
 	arr.forEach((item) => {
 		let currentValue = arr[0];
@@ -39,6 +37,8 @@ let arrayRemove = (arr, value) => {
 
 // Check if value repeats in array
 let cleanArray = (arr) =>{
+	// sort array in ascending order:
+	arr = arr.sort((a, b) => a-b);
 	arr.forEach((item) => {
 		if(arr.length>0) {
 			arrayFind(arr);
@@ -47,25 +47,40 @@ let cleanArray = (arr) =>{
 			// clear tempArr
 			tempArr = [];
 			// console.log(newArr);
-
 		}
 	})
-	return newArr;
+	// return newArr;
 }
 
+// initial function
 const init = (arr => {
+	// clear newArr for when init is called to start fresh.
+	newArr = [];
+	// get string types in one array
 	strArr = arr.filter(item => {
 		return typeof item === 'string';
 	})
-
+	// get number types in one array
 	numArr = arr.filter(item => {
 		return typeof item === 'number';
 	})
-	cleanArray(numArr);
-	cleanArray(strArr);
-	newArr.push(numArr);
-	newArr.push(strArr);
-	
+	// if there are both types of arrays run cleanArray on both arrays and then push into newArr
+	if((numArr.length>1) && (strArr.length>1)) {
+		cleanArray(numArr);
+		cleanArray(strArr);
+		newArr = [];
+		newArr.push(numArr);
+		newArr.push(strArr);
+		return newArr;
+		// if there is only a number array run cleanArray and return newArr
+	} else if((numArr.length>1) && !(strArr.length>1)) {
+		cleanArray(numArr);
+		return newArr;
+		// if there is only a number array run cleanArray and return newArr
+	} else if(!(numArr.length>1) && (strArr.length>1)) {
+		cleanArray(strArr);
+		return newArr;
+	}
 })
 
 // return new organised array
